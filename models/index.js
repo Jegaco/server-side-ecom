16ce259ecf0935d9ca818038dc3dbdc9d295dd4f
Jegaco <jeffgcorpuz@gmail.com>
2023-06-06 09:17:40 -0700

    Defined fields for all models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catetgory_id'
+})
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'cascade'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  onDelete: 'cascade'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'cascade'
+})
 
 module.exports = {
   Product,
